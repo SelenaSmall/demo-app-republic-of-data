683f8d4710e25eeffac831e076590c0800977fe0
selenasmall <selenawiththetattoo@gmail.com>
2018-08-15 21:55:51 +1000

    get an insight

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Insight from './Insight'
 
 class App extends Component {
+    componentDidMount() {
+        fetch('/api/melbourne_traffic')
+            .then(response => response.json())
+            .then(data => this.setState({ data }))
+    }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Republic of Data</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+        <div className="App">
+            <Insight data={this.state}></Insight>
+        </div>
+    )
+
+
   }
 }
 
---
+++ client/src/Insight.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+
+const Insight =({data})=> `🖕 ${JSON.stringify(data)}`
+
+
+export default Insight
