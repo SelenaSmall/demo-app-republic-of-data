a12ad061b0ba837d211310c90b2e2e01ae146660
Michael Milewski <saramic@gmail.com>
2018-08-20 23:16:52 +1000

    reformatted and moved d3js into own demo component

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
-import Insight from './Insight'
+import D3JsDemo from './insights/D3JsDemo'
 import ChartJsDemo from "./insights/ChartJsDemo";
 
 class App extends Component {
-    state = { data: [] }
-    componentDidMount() {
-        fetch('/api/melbourne_traffic')
-            .then(response => response.json())
-            .then(data => this.setState({ data }))
-    }
+  state = {data: []}
+
+  componentDidMount() {
+    fetch('/api/melbourne_traffic')
+      .then(response => response.json())
+      .then(data => this.setState({data}))
+  }
+
   render() {
     return (
-        <div className="App">
+      <div className="App">
 
-            <div className="App-header">
-                <h2>Republic of Data - Insights</h2>
-            </div>
-            <Insight data={this.state.data.map((d)=>d.mean_speed)} size={[500,500]}></Insight>
-            <ChartJsDemo/>
+        <div className="App-header">
+          <h2>Republic of Data - Insights</h2>
         </div>
+        <ChartJsDemo/>
+        <D3JsDemo data={this.state.data.map((d) => d.mean_speed)} size={[500, 500]}></D3JsDemo>
+      </div>
     )
 
 
